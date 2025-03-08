3be4b586188b7e434e16b6ef2bbf2ecde97171d5
subhashchandra-26 <subhashchandra.1026@gmail.com>
2025-03-08 16:14:16 +0530

    Update Home.jsx

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { AuthContext } from '../context/AuthContext'
+import { AuthContext } from '../context/Authcontext'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
